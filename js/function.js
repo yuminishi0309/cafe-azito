c2d0f18079684a7f64ba51fdd4208a32c11f3551
YumiN <yuminishi0309@gmail.com>
2023-08-16 23:34:08 +0900

    add: header mv layout

--- js/function.js
+++ js/function.js
@@ -1,5 +1,20 @@
 $(function() {
-// ここにjQueryを記述
+
+// メニューボタン
+$('#btn').on('click', function() {
+    $('#btn__top').toggleClass('rotateTop');
+    $('#btn__middle').toggleClass('hideMiddle');
+    $('#btn__bottom').toggleClass('rotateBottom');
+    $('#gnav').toggleClass('gnav-fadeIn');
+});
+
+// ナビゲーション操作
+$('.gnav__link').on('click', function () {
+    $('#btn__top').removeClass('rotateTop');
+    $('#btn__middle').removeClass('hideMiddle');
+    $('#btn__bottom').removeClass('rotateBottom');
+    $('#gnav').removeClass('gnav-fadeIn');
+});
 
 
 
