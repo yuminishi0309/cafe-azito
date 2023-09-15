3ecc53ce27ead4568cea82a6d3941f376ad58965
YumiN <yuminishi0309@gmail.com>
2023-09-15 23:46:54 +0900

    add: swiper, add: footer layout

--- js/function.js
+++ js/function.js
@@ -1,24 +1,19 @@
 $(function() {
 
-// メニューボタン
-$('#btn').on('click', function() {
-    $('#btn__top').toggleClass('rotateTop');
-    $('#btn__middle').toggleClass('hideMiddle');
-    $('#btn__bottom').toggleClass('rotateBottom');
-    $('#gnav').toggleClass('gnav-fadeIn');
-});
+    // メニューボタン
+    $('#btn').on('click', function() {
+        $('#btn__top').toggleClass('rotateTop');
+        $('#btn__middle').toggleClass('hideMiddle');
+        $('#btn__bottom').toggleClass('rotateBottom');
+        $('#gnav').toggleClass('gnav-fadeIn');
+    });
 
-// ナビゲーション操作
-$('.gnav__link').on('click', function () {
-    $('#btn__top').removeClass('rotateTop');
-    $('#btn__middle').removeClass('hideMiddle');
-    $('#btn__bottom').removeClass('rotateBottom');
-    $('#gnav').removeClass('gnav-fadeIn');
-});
-
-
-
-
-
+    // ナビゲーション操作
+    $('.gnav__link').on('click', function () {
+        $('#btn__top').removeClass('rotateTop');
+        $('#btn__middle').removeClass('hideMiddle');
+        $('#btn__bottom').removeClass('rotateBottom');
+        $('#gnav').removeClass('gnav-fadeIn');
+    });
 
 });
---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+const swiperInsta = new Swiper('.swiper-insta', {
+    direction: 'horizontal',
+    loop: true,
+    effect: 'slide',
+    slidesPerView: 4,
+    speed: 6000,
+    autoplay : {
+        delay: 300,
+    }
+});
