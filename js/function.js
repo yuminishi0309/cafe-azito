a319d11c1bb067e5421b571192da29c8f8c498b8
YumiN <yuminishi0309@gmail.com>
2023-10-05 18:33:24 +0900

    add: responsive setting in swiper, fix: change jquery to js description, the link to display correctly in info-sec

--- js/function.js
+++ js/function.js
@@ -1,19 +1,25 @@
-$(function() {
+// フェードインメニューボタン設定-------------------------------------------
+const menuBtn = document.querySelector('#btn');
+const btnTop = document.querySelector('#btn__top');
+const btnMiddle = document.querySelector('#btn__middle');
+const btnBottom = document.querySelector('#btn__bottom');
+const gnav = document.querySelector('#gnav');
 
-    // メニューボタン
-    $('#btn').on('click', function() {
-        $('#btn__top').toggleClass('rotateTop');
-        $('#btn__middle').toggleClass('hideMiddle');
-        $('#btn__bottom').toggleClass('rotateBottom');
-        $('#gnav').toggleClass('gnav-fadeIn');
-    });
-
-    // ナビゲーション操作
-    $('.gnav__link').on('click', function () {
-        $('#btn__top').removeClass('rotateTop');
-        $('#btn__middle').removeClass('hideMiddle');
-        $('#btn__bottom').removeClass('rotateBottom');
-        $('#gnav').removeClass('gnav-fadeIn');
-    });
-
+// メニューボタン操作時の挙動
+menuBtn.addEventListener('click', () => {
+    btnTop.classList.toggle('rotateTop');
+    btnMiddle.classList.toggle('hideMiddle');
+    btnBottom.classList.toggle('rotateBottom');
+    gnav.classList.toggle('gnav-fadeIn');
 });
+
+// ナビゲーション操作時の挙動
+const gnavLinks = document.querySelectorAll('.gnav__link');
+for (let i =0; i < gnavLinks.length; i++) {
+    gnavLinks[i].addEventListener('click', () => {
+        btnTop.classList.remove('rotateTop');
+        btnMiddle.classList.remove('hideMiddle');
+        btnBottom.classList.remove('rotateBottom');
+        gnav.classList.remove('gnav-fadeIn');
+    });
+}
--- js/swiper.js
+++ js/swiper.js
@@ -10,3 +10,24 @@ const swiperInsta = new Swiper('.swiper-insta', {
         disableOnInteraction: false,
     }
 });
+
+// swiperレスポンシブ対応--------------------------------------------
+function updateSwiper() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth >= 1024) {
+        swiperInsta.params.slidesPerView = 6;
+    } else if (screenWidth >= 768) {
+        swiperInsta.params.slidesPerView = 4;
+    } else {
+        swiperInsta.params.slidesPerView = 2;
+    }
+    swiperInsta.update();
+}
+
+// 初回の設定
+updateSwiper();
+
+// 画面サイズ変更時の処理
+window.addEventListener('resize', function() {
+    updateSwiper();
+});
